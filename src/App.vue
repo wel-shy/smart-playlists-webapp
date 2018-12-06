641d1b2f75360dfd3061aee1d2e672db05a993c5
Daniel Welsh <d.welsh@newcastle.ac.uk>
2018-12-06 16:22:00 +0000

    :tada: Initialising from webapp in api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  div#app
+    div.container-fluid
+      section.hero.is-light.header
+        div.hero-body
+          div.container
+            h1.title Smart Playlists
+            h2.subtitle Auto generate smart playlists for Spotify.
+    Login(
+      v-if="!isLoggedIn"
+    )
+    Home(
+      v-else,
+      v-on:logged-out="isLoggedIn = false"
+    )
+    FooterBar
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Login from './views/Login.vue';
+import Home from './views/Home.vue';
+import FooterBar from './views/FooterBar.vue';
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld,
+    Login,
+    Home,
+    FooterBar,
+  },
+  data() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  mounted() {
+    if (this.token) {
+      this.isLoggedIn = true;
+      return;
+    }
+
+    const urlString = window.location.href;
+    const url = new URL(urlString);
+    const authToken = url.searchParams.get('authToken');
+
+    // If url contains an authToken then store a cookie and redirect to root.
+    if (authToken) {
+      this.setCookie(1, authToken);
+      // eslint-disable-next-line prefer-destructuring
+      window.location.href = window.location.href.split('?')[0];
+    }
+  },
+  methods: {
+    setCookie(expireDays, value) {
+      const expires = new Date();
+      expires.setDate(expires.getDate() + 1);
+      document.cookie = `${encodeURIComponent('authToken')}=${`${encodeURIComponent(value)}; expires=${expires}`}`;
+    },
+    getCookie(name) {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) {
+        return parts.pop().split(';').shift();
+      }
+      return null;
+    },
+  },
+  computed: {
+    token() {
+      return this.getCookie('authToken');
+    },
   },
 };
 </script>
 
 <style lang="scss">
+  @import "~bulma";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+  .header {
+    min-height: 25vh;
+    padding-top: 5%;
+  }
 </style>
